82d5831ab7674f719a26b124ee3fbda44ee523b7
Elaine Delgado <delgado.elaine03@gmail.com>
2021-02-25 18:23:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Dev-Finance 📈
+
+### _Projeto desenvolvido durante a Maratona Discover da Rocketseat_ 🚀 🚀 🚀
+
+&nbsp;
+
+---
+
+## O que é:
+
+&nbsp;
+
+#### _Dev-Finance é um aplicativo para controle financeiro, onde podemos adicionar entradas e saídas._
+
+&nbsp;
+
+---
+
+## Tecnologias utilizadas:
+
+&nbsp;
+
+-   HTML
+-   CSS
+-   Javascript
+    &nbsp;
+
+---
+
+## Layout:
+
+#### _As cores do layout foram modificadas, os tons de verde do projeto original foram substituídas por tons de azul._
+
+&nbsp;
+
+##### _Tela do aplicativo:_
+
+![](assets/img-layout/devfinanceRM.png)
+&nbsp;
+
+##### _Tela do modal para adicionar nova Transação:_
+
+![](assets/img-layout/modalRM.png)
+&nbsp;
+
+---
+
+## Modo dark-theme:
+
+#### _Adicionei ao projeto um dark-theme, ou dark-mode, que não havia no original._
+
+&nbsp;
+
+##### _Tela do aplicativo:_
+
+![](assets/img-layout/devfinanRMdark.png)
+&nbsp;
+
+##### _Tela do modal, versão dark-theme, para adicionar nova Transação:_
+
+![](assets/img-layout/modaldarkRM.png)
+
+---
+
+## Modo responsivo
+
+#### _ O aplicativo tem sua versão responsiva para mobile e tablets: _
+
+&nbsp;
+
+##### _Aparência do aplicativo na versão mobile entre 320px e 720px:_
+
+![](assets/img-layout/devfinanresponsivo.png) <img height="350" hspace="20"/> ![](assets/img-layout/devfinanresponsivodark.png)
+
+---
+
+### Projeto feito por 🌸Elaine Delgado🌸 durante Maratona Discover 🚀 🚀 🚀
---
+++ js/script.js
@@ -0,0 +1,261 @@
+/* Modal ==== */
+/*
+function initModal() {  
+    const modal = document.querySelector('.modal-overlay')
+    const openModalButton = document.querySelector('a.new')
+    const closeModalButton = document.querySelector('a.cancel')
+
+    function toggleModal() {
+        modal.classList.toggle('active')
+    }
+    
+    openModalButton.addEventListener('click', toggleModal)
+    closeModalButton.addEventListener('click', toggleModal)
+}
+initModal();*/
+
+
+const Modal = {
+    open(){
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+
+    },
+    close(){
+        // fechar o modal
+        // remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
+    }
+}
+
+/* Transacoes ===============*/
+
+const Storage = {
+    get() {
+      return JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
+    },
+
+    set(transactions) {
+        localStorage.setItem('dev.finances:transactions', JSON.stringify(transactions))
+    }
+}
+
+const Transaction = {
+    all: Storage.get(),
+   
+
+    add(transaction) {
+        Transaction.all.push(transaction)
+        
+        App.reload();
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1);
+        
+        App.reload();
+    },
+
+    incomes() {
+        let income = 0;
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount; 
+            }
+        })
+        return income;
+    },
+    expenses() {
+        let expense = 0;
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount; 
+            }
+        })
+        return expense;
+    },
+    total() {
+        return Transaction.incomes() + Transaction.expenses();
+    } 
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index){
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index  = index
+        
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction, index) {
+
+        const cssClass = transaction.amount > 0 ? 'income' : 'expense'
+
+       const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+                    <td class="description">${transaction.description}</td>
+                    <td class="${cssClass}">${amount}</td>
+                    <td class:="date">${transaction.date}</td>
+                    <td><img onclick="Transaction.remove(${index})" src="assets/minus.svg" alt=""></td>                   
+                    `
+                    return html
+    },
+
+    updateBalance() {
+        document.querySelector('#incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes())   
+    
+        document.querySelector('#expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())  
+
+        document.querySelector('#totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total()) 
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ''
+    }
+}
+
+/* Formatando a moeda:  */
+const Utils = {
+    formatAmount(value){
+        value = value * 100
+        return Math.round(value)
+    },
+
+    formatDate(date) {
+        const splittedDate = date.split('-')
+        return  `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? '-' : '' //se o valor for negativo adiciona sinal -
+
+        value = String(value).replace(/\D/g, '') //remove qualquer caeactere que nao seja numero
+
+        value = Number(value) /100 //divide por cem para criar os centavos
+
+        value = value.toLocaleString("pt-BR", {
+            style: 'currency',
+            currency:'BRL'
+        })
+
+        return signal + value;
+    }
+}
+
+/* Pegando dados dos formulários */
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value, 
+            amount: Form.amount.value, 
+            date: Form.date.value,
+        }
+    },
+    
+    validateFields() {
+        const { description, amount, date }= Form.getValues()
+        
+        if (description.trim() === '' || amount.trim() === '' || date.trim() === '') {
+            throw new Error('Por favor, preencha todos os campos.')
+        }
+    },
+
+    formatValues() {
+        let{ description, amount, date } = Form.getValues()        
+        amount = Utils.formatAmount(amount)
+        date = Utils.formatDate(date)
+        return {
+            description,
+            amount,
+            date,
+        }
+    },
+
+    clearFields() {
+      Form.description.value = '' 
+      Form.amount.value = '' 
+      Form.date.value = '' 
+    },
+
+    submit(event) {
+        event.preventDefault();
+
+        try {
+            Form.validateFields()
+            const transaction =  Form.formatValues()
+            Transaction.add(transaction)// salvar
+            Form.clearFields() // limpar formulario
+            Modal.close()           
+        } catch (error) {
+            alert(error.message)
+        }              
+    },
+}
+
+
+
+
+
+const App = {
+    init() {
+        Transaction.all.forEach((transaction, index) =>  {
+            DOM.addTransaction(transaction, index)
+})
+/* ou
+* Transaction.all.forEach(DOM.addTransaction)
+*/
+        DOM.updateBalance();
+
+        Storage.set(Transaction.all)
+},
+
+    reload() {
+        DOM.clearTransactions();
+        App.init();
+    },
+}
+
+App.init();
+
+
+/* ========= Dark Mode ========*/
+//Pegando os elementos e colocando em variávies
+const btnTheme = document.querySelector('.btn-toggle')
+const imgTheme =  document.querySelectorAll('.btn-toggle img')//[img.moon.active, img.sun]
+
+
+//Adicionando/removendo class dark-mode ao html
+btnTheme.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark-mode')    
+})
+
+//Função para mudar imagem do botão dark-mode de acordo com o tema escolhido
+function activeItem() {
+    imgTheme.forEach(image => {
+        image.classList.toggle('active')
+    })
+}
+
+imgTheme.forEach(item => {
+    item.addEventListener('click', activeItem)
+})
+
+
+
+
+
